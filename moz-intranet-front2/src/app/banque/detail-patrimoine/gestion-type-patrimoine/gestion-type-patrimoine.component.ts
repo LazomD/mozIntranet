03c6fbc5f7484cde89428ce643bff084b2d62997
lazom <lazom.dzemaili@gmail.com>
2019-04-12 22:15:15 +0200

    page gestion de type du patrimoine

--- moz-intranet-front2/src/app/banque/detail-patrimoine/gestion-type-patrimoine/gestion-type-patrimoine.component.ts
+++ moz-intranet-front2/src/app/banque/detail-patrimoine/gestion-type-patrimoine/gestion-type-patrimoine.component.ts
@@ -1,11 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+interface typePatrimoine {
+  name: string;
+  id: number;
+}
+
+const TYPES_PATRIMOINE: typePatrimoine[] = [
+  {
+    name: 'PEL',
+    id : 1
+  },
+  {
+    name: 'Livret A',
+    id : 2
+  },
+  {
+    name: 'BNP',
+    id : 3
+  },
+  {
+    name: 'SG',
+    id : 4
+  },
+  {
+    name: 'Test',
+    id : 5
+  }
+];
+
 @Component({
   selector: 'app-gestion-type-patrimoine',
   templateUrl: './gestion-type-patrimoine.component.html',
   styleUrls: ['./gestion-type-patrimoine.component.css']
 })
 export class GestionTypePatrimoineComponent implements OnInit {
+
+  typesPatrimoine: typePatrimoine[] = TYPES_PATRIMOINE;
 
   constructor() { }
 
